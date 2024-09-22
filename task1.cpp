3fa5370b409ee3f730930b845a0b841efa82c92a
abubakarasif7207 <abubakarprofessional7207@gmail.com>
2024-09-22 17:16:37 +0500

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main(){
+ int sides;
+cout<<"Enter the number of the sides of the polygon: ";
+cin>>sides;
+int result=(sides-2)*180;
+cout<<"the sum of internal angles of a 40 sided polygon is: "<<result<<"  degrees";
+}
