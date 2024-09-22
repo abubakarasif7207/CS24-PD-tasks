6f59082cadefc900faa6961ea59d36a59372599e
abubakarasif7207 <abubakarprofessional7207@gmail.com>
2024-09-22 17:18:27 +0500

    Add files via upload

---
+++ task4.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+main(){
+float imposter;
+cout<<"enter imposter count :";
+cin>>imposter;
+float player;
+cout<<"enter player count :";
+cin>>player;
+float result;
+result=100*(imposter/player);
+cout<<"change of being an imposter : "<<result<<"%"<<endl;
+float imposter2;
+cout<<"enter imposter count :";
+cin>>imposter2;
+float player2;
+cout<<"enter player count :";
+cin>>player2;
+float result2;
+result2=100*(imposter2/player2);
+cout<<"change of being an imposter : "<<result2<<"%"<<endl;
+float imposter3;
+cout<<"enter imposter count :";
+cin>>imposter3;
+float player3;
+cout<<"enter player count :";
+cin>>player3;
+float result3;
+result3=100*(imposter3/player3);
+cout<<"change of being an imposter : "<<result3<<"%";
+}
+
