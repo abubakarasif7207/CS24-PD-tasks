da59eab9de968a687b9d058dd48842e7fe655bfe
abubakarasif7207 <abubakarprofessional7207@gmail.com>
2024-09-22 17:21:05 +0500

    Add files via upload

---
+++ task11.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+main(){
+int age;
+cout<<"enter the person's age: ";
+cin>>age;
+int move;
+cout<<"enter the numbers of times they've moved : ";
+cin>>move;
+int result;
+result=age/(move+1);
+cout<<"average number of years lived in the saingle house : "<<result<<endl;
+int age2;
+cout<<"enter the person's age: ";
+cin>>age2;
+int move2;
+cout<<"enter the numbers of times they've moved : ";
+cin>>move2;
+int result2;
+result2=age2/(move2+1);
+cout<<"average number of years lived in the saingle house : "<<result2<<endl;
+int age3;
+cout<<"enter the person's age: ";
+cin>>age3;
+int move3;
+cout<<"enter the numbers of times they've moved : ";
+cin>>move3;
+int result3;
+result3=age3/(move3+1);
+cout<<"average number of years lived in the saingle house : "<<result3;
+}
+
+
+
